b1b76983c592d748ec5ece0eac3467ba0c1de0a2
Wei Yan <weiyan0523@gmail.com>
2022-11-30 16:23:30 -0600

    update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
-
 function Login() {
   return (
+    
     <div>
-        <Navbar/>
+      <Navbar/>
+      <div>
       <h1 className='Log-in'>Log In</h1>
+      <form action="process-inc.php" method="post">
       <label for = "userName"><b>Username</b></label>
       <input type = "text" placeholder="Enter Username" name = "userName" required></input>
 
       <label for = "psw"><b>Password</b></label>
       <input type = "password" placeholder="Enter Password" name = "psw" required></input>
       <br></br>
-      <button type="submit">Click to Login</button>
-      
+      <button>Click to Login</button>
+      </form>
+      </div>
+
+    
     </div>
+    
   )
 }
 
---
+++ src/Pages/process-inc.php
@@ -0,0 +1,19 @@
+<?php
+
+// // $servername = "localhost";
+// // $dBUsername = "root";
+// // $dBPassword = "admin12345";
+// // $dBName = "loginsystem";
+
+
+// // $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+// // if(!$conn){
+// //     die("Connection failed: ".mysqli_connect_error());
+// // }
+// $userName = $_POST["userName"]
+// $psw = $_POST["psw"]
+
+
+// var_dump($userName,$psw);
+print_r($_POST)
